a619926faf96e145457b32219604daaf9d4cdfcc
Константин <kuz18rus@ya.ru>
2022-12-05 11:17:24 +0400

    Задание 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Задача 1
-        System.out.println("Задача 1");
+        // Задание 1 и 2
+        System.out.println("Задание 1 и 2");
         int clientOS = 1;
-        if (clientOS == 0) {
+        int year = 2010;
+        if (clientOS == 0)
+            if (year <= 2015){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (year > 2015){
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1){
-            System.out.println("Установите версию приложения для Android по ссылке");
         }
+        if (clientOS == 1)
+            if (year <= 2015){
+            System.out.println("Установите облегченную версию приложения для android по ссылке");
+        }else if (year > 2015){
+                System.out.println("Установите версию приложения для android по ссылке");
+            }
 
-    }
+   }
 }
