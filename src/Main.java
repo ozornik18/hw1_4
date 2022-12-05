42449685e462fd2b74230f4b44fe576b85e34bf4
Константин <kuz18rus@ya.ru>
2022-12-05 12:23:15 +0400

    Задание 4-5 выполнено

--- src/Main.java
+++ src/Main.java
@@ -58,6 +58,52 @@ public class Main {
             default:
               System.out.println("год не является високосным");
             }
+        // Задание 4
+        System.out.println("Задание 4");
+        int deliveryDistance = 59; //км
+        int day = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + day);
+        } else if (deliveryDistance < 60) {
+            System.out.println("Потребуется дней: " + day * 2);
+        } else if (deliveryDistance < 100) {
+            System.out.println("Потребуется дней: " + day * 3);
+        } else if (deliveryDistance < 140) {
+            System.out.println("Потребуется дней: " + day * 4);
+        } else if (deliveryDistance < 160) {
+            System.out.println("Потребуется дней: " + day * 5);
+        }
+
+// Задание 5
+        System.out.println("Задание 5");
+        int monthNumber = 15;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("месяц принадлежит к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("месяц принадлежит к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("месяц принадлежит к сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("месяц принадлежит к сезону осень");
+                break;
+            default:
+                System.out.println("такого месяца не существует");
+        }
+
+
+
 
    }
 }
